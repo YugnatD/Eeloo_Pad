7bb385838e608af621b7642e5ce084bcd7d49a29
Tanguy Dietrich <tanguydietrich@gmail.com>
2023-03-14 18:31:32 +0100

    Adding Project for KSP1

--- plugin/KSP1/PluginEelooPAD/PluginEelooPAD/PluginEelooPAD.cs
+++ plugin/KSP1/PluginEelooPAD/PluginEelooPAD/PluginEelooPAD.cs
@@ -1,19 +1,20 @@
-using System;
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Net.Sockets;
 using System.Xml;
 using UnityEngine;
 using System.Runtime.InteropServices;
 
- 
+
 namespace PluginEelooPAD
 {
     // template for data to send
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct DataToSend 
+    public struct DataToSend
     {
         public byte id;
         public float AP;
@@ -28,26 +29,29 @@ namespace PluginEelooPAD
         private Socket sender;
         private XmlDocument xmlConfig = new XmlDocument();
         private IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
-        private IPAddress ipAddr = ipHost.AddressList[0];
+        private IPAddress ipAddr;
         private IPEndPoint localEndPoint;
         public static Vessel ActiveVessel = new Vessel();
 
-        public DataToSend dataToSend; // Will contain 
+        public DataToSend dataToSend; // Will contain data of the vessel
         //called on startup of the plugin by Unity
         void Awake()
         {
+            ipAddr = ipHost.AddressList[0];
             // read XML configuration file
             xmlConfig.Load("PluginEelooPAD.xml");
             // TODO: read XML configuration file
-            localEndPoint = new IPEndPoint("127.0.0.1", 11111);
+            localEndPoint = new IPEndPoint(ipAddr, 11111);
             // init a socket to send data to
             sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            try {
+            try
+            {
                 sender.Connect(localEndPoint);
                 byte[] messageSent = Encoding.ASCII.GetBytes("INIT");
                 int byteSent = sender.Send(messageSent);
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
                 print("KSPSerialIO: Error connecting to socket: " + e.ToString());
             }
             print("KSPSerialIO: Loading settings and socket connected...");
@@ -97,6 +101,8 @@ namespace PluginEelooPAD
         //called once per frame
         void Update()
         {
-            settingsConfig.dataToSend.AP = (float)ActiveVessel.orbit.ApA;
+            //settingsConfig.dataToSend.AP = (float)settingsConfig.ActiveVessel.orbit.ApA;
         }
+    }
+
 }
