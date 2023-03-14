da6592bab0fd3fcadbae365b27bbd5f39a8690c1
Tanguy Dietrich <tanguydietrich@gmail.com>
2023-03-14 16:18:54 +0100

    Adding a template for the communication mod

---
+++ plugin/KSP2/PluginEelooPAD.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Xml;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+ 
+namespace PluginEelooPAD
+{
+    // template for data to send
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct DataToSend 
+    {
+        public byte id;
+        public float AP;
+        public byte M1;
+        public byte M2;
+        public byte M3;
+    }
+
+    //
+    public class SettingsConfig : MonoBehaviour
+    {
+        private Socket sender;
+        private XmlDocument xmlConfig = new XmlDocument();
+        private IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+        private IPAddress ipAddr = ipHost.AddressList[0];
+        private IPEndPoint localEndPoint;
+        public static Vessel ActiveVessel = new Vessel();
+
+        public DataToSend dataToSend; // Will contain 
+        //called on startup of the plugin by Unity
+        void Awake()
+        {
+            // read XML configuration file
+            xmlConfig.Load("PluginEelooPAD.xml");
+            // TODO: read XML configuration file
+            localEndPoint = new IPEndPoint("127.0.0.1", 11111);
+            // init a socket to send data to
+            sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try {
+                sender.Connect(localEndPoint);
+                byte[] messageSent = Encoding.ASCII.GetBytes("INIT");
+                int byteSent = sender.Send(messageSent);
+            }
+            catch (Exception e) {
+                print("KSPSerialIO: Error connecting to socket: " + e.ToString());
+            }
+            print("KSPSerialIO: Loading settings and socket connected...");
+        }
+
+        public void SendStruct()
+        {
+            // send data to socket
+            byte[] messageSent = StructureToByteArray(dataToSend);
+            int byteSent = sender.Send(messageSent);
+        }
+
+        //THANKS KSPSERIALIO
+        private static byte[] StructureToByteArray(object obj)
+        {
+            int len = Marshal.SizeOf(obj);
+            byte[] arr = new byte[len];
+            IntPtr ptr = Marshal.AllocHGlobal(len);
+            Marshal.StructureToPtr(obj, ptr, true);
+            Marshal.Copy(ptr, arr, 0, len);
+            Marshal.FreeHGlobal(ptr);
+            return arr;
+        }
+
+        private static object ByteArrayToStructure(byte[] bytearray, object obj)
+        {
+            int len = Marshal.SizeOf(obj);
+            IntPtr i = Marshal.AllocHGlobal(len);
+            Marshal.Copy(bytearray, 0, i, len);
+            obj = Marshal.PtrToStructure(i, obj.GetType());
+            Marshal.FreeHGlobal(i);
+            return obj;
+        }
+    }
+
+    public class UpdateDataKSP : MonoBehaviour
+    {
+        public SettingsConfig settingsConfig = new SettingsConfig();
+
+        //called on startup of the plugin by Unity
+        void Awake()
+        {
+            //init a timer to send data to the socket
+
+        }
+
+        //called once per frame
+        void Update()
+        {
+            settingsConfig.dataToSend.AP = (float)ActiveVessel.orbit.ApA;
+        }
+}
